156d345e9f467d8bb436fc329f0500b29e9eedf7
Alexey <105539363+Hoobbaaboobba@users.noreply.github.com>
2023-01-28 21:47:29 +0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,24 @@
+const searchButton = document.querySelector(".nav_buttons .link-search");
+const closeButton = document.querySelector(".search-container .link-close");
+const desktopNav = document.querySelector(".nav_buttons");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
+
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
